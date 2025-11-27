dd5728951d6f6c7881f230f57428bee48aa2fc4b
Numsik <honzik.klusek.myto@gmail.com>
2025-11-27 13:26:37 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import LeftPanel from "./functionality.jsx"
+import './app.css'
+function App() {
+
+  return (
+    <>
+      <h1 className="heading">CV Generator</h1>
+      <div>
+        <LeftPanel></LeftPanel>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/functionality.jsx
@@ -0,0 +1,23 @@
+import "./leftpanel.css"
+
+function LeftPanel(){
+    return(
+        <>
+        <div className="wholeass">
+
+            <h1 className="general">General Information</h1>
+        
+            <div className="inputy">
+                <p>Full Name</p>
+                <input placeholder="fullname"/> 
+            </div>
+
+
+        </div>
+       
+        </>
+    )
+
+}
+
+export default LeftPanel;
